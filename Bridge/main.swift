12650fb490c2d082b4a63df1f97b571e1195f846
Grayson <info@fromconcentratesoftware.com>
2016-12-09 08:21:57 -0500

    Remove `@NSApplicationMain` tag and add main.swift to Bridge

---
+++ Bridge/main.swift
@@ -0,0 +1,9 @@
+//
+//  main.swift
+//  InterprocessCommunication
+//
+//  Created by Grayson Hansard on 12/9/16.
+//  Copyright © 2016 From Concentrate Software. All rights reserved.
+//
+
+import Foundation
