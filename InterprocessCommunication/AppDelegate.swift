23a5082a7dd0845714220d564005dce6dd392c08
Grayson <info@fromconcentratesoftware.com>
2016-12-09 08:11:19 -0500

    Create Xcode project

---
+++ InterprocessCommunication/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  InterprocessCommunication
+//
+//  Created by Grayson Hansard on 12/9/16.
+//  Copyright © 2016 From Concentrate Software. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
